6a19af72511a88b2a63f6a48ee77fa9ec679ec4b
joyaljoynk <joyaljoy0601@gmail.com>
2025-08-21 15:48:59 +0530

    first commit

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,28 +1,29 @@
-export default function skills() {
-    return(
-        <div className="skills-section">
-        <section className="skills-column"> 
-        <h1 className="he-right">Skills </h1>
+export default function Skills() {
+  return (
+    <div className="skills-section">
+        <h1 className="he-right">Skills</h1>
+      <section className="skills-column"> 
+        
         <h2 className="tech">TECHNICAL</h2>
         <ul>
-            <li className="p">Programming Languages: C, Python, MySQL, Java,</li>
-            <li className="p">Cyber Security : SOC level 1</li>
-            <li className="p">Game Development,A.R.,V.R :unity</li>
-            <li className="p">Web Development: HTML, CSS, JavaScript, React</li>
-            <li className="p">3D Modelling: Blender</li>  
+          <li className="p">Programming Languages: C, Python, MySQL, Java</li>
+          <li className="p">Cyber Security: SOC level 1</li>
+          <li className="p">Game Development: AR, VR (Unity)</li>
+          <li className="p">Web Development: HTML, CSS, JavaScript, React</li>
+          <li className="p">3D Modelling: Blender</li>  
         </ul>
-        </section>
-        <section className="'skills-columns">
+      </section>
+
+      <section className="skills-column">
         <h2 className="tech">SOFT SKILLS</h2>
         <ul>        
-            <li className="p">Problem Solving</li>
-            <li className="p">Teamwork</li>
-            <li className="p">Communication</li>    
-            <li className="p">Adaptability</li>
-            <li className="p">Time Management</li>
+          <li className="p">Problem Solving</li>
+          <li className="p">Teamwork</li>
+          <li className="p">Communication</li>    
+          <li className="p">Adaptability</li>
+          <li className="p">Time Management</li>
         </ul>
-        </section>
-        </div>
-    
-    );
+      </section>
+    </div>
+  );
 }
